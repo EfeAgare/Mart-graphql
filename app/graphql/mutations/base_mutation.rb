8d2ae766885948d8dd031d3e22f7329ddda9c321
Efe Agare <knowledgeagare157@gmail.com>
2023-01-09 23:47:03 +0100

    add customer register mutation

--- app/graphql/mutations/base_mutation.rb
+++ app/graphql/mutations/base_mutation.rb
@@ -1,8 +1,18 @@
 module Mutations
   class BaseMutation < GraphQL::Schema::RelayClassicMutation
+    include ErrorMessage
+
     argument_class Types::BaseArgument
     field_class Types::BaseField
     input_object_class Types::BaseInputObject
     object_class Types::BaseObject
+
+    def self.authorized?(object, context)
+      unless context[:current_user]
+        raise GraphQL::ExecutionError.new("Unauthorized error", options: { status: :unauthorized, code: 401 })
+      end
+
+      super && context[:current_user]
+    end
   end
 end
