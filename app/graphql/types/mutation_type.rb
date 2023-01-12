854eca46a31103da213085a528cb3b4c871a3116
Efe Agare <knowledgeagare157@gmail.com>
2023-01-12 14:24:43 +0100

    write category spec

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -7,5 +7,13 @@ module Types
     field :update_customer_address, mutation: Mutations::Customer::UpdateCustomerAddress
     field :update_customer_credit_card, mutation: Mutations::Customer::UpdateCustomerCreditCard
     field :update_customer_details, mutation: Mutations::Customer::UpdateCustomerDetails
+
+    # Department
+    field :create_department, mutation: Mutations::Department::CreateDepartment
+
+
+    # Category
+    field :create_category, mutation: Mutations::Category::CreateCategory
+    
   end
 end
