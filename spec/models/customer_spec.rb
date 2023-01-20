890388d0b7ae6a9c768941b7a40368ba0d837884
Efe Agare <knowledgeagare157@gmail.com>
2023-01-20 18:01:23 +0100

    add spec test

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Customer, type: :model do
   describe "Validation" do
     it { should validate_presence_of(:name) }
   end
+
+  describe 'associations' do
+    it { should have_many(:reviews) }
+  end
 end
