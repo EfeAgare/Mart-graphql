77733671b17b5d762213d5d56f76dae2a982dc13
Efe Agare <knowledgeagare157@gmail.com>
2023-01-20 17:58:01 +0100

    use pg_search and kaminari for pagination

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,17 @@
 class Product < ApplicationRecord
   has_many :product_categories
   has_many :order_details
+  has_many :reviews
+
+  paginates_per 25
+
+  include PgSearch::Model
+  
+  pg_search_scope :search,
+                  against: [:name, :description],
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 
   validates :name, presence: true
   validates :description, presence: true
