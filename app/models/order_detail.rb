1a7b97d51aa2069fe10e2eddef1920ffff22bf78
Efe Agare <knowledgeagare157@gmail.com>
2023-02-10 14:56:11 +0100

    move sql to model

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -6,6 +6,19 @@ class OrderDetail < ApplicationRecord
   validates :product_name, presence: true
   validates :unit_cost, presence: true
 
+  scope :fetch, -> (order_id) {find_by_sql(["SELECT order_id, product_id, attributes, product_name,
+         quantity, unit_cost, (quantity * unit_cost) AS subtotal FROM   order_details 
+         WHERE  order_id = ?", order_id])}
+
+  scope :create_order, -> (id, cart_id) {
+    find_by_sql(["INSERT INTO order_details 
+        (?, product_id, attributes, product_name, quantity, unit_cost)
+        SELECT ?, p.id, sc.attributes, p.name, sc.quantity,
+        COALESCE(NULLIF(p.discounted_price, 0), p.price) AS unit_cost
+        FROM shopping_carts sc INNER JOIN  products p ON sc.product_id = p.id
+        WHERE  sc.cart_id = ? AND sc.buy_now", id, cart_id])
+  }
+  
   def self.instance_method_already_implemented?(method_name)
     return true if %w(frozen frozen?).include? method_name 
   end
