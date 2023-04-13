f5a4325919845c877e465811ab81e069f6e17e7c
Efe Agare <knowledgeagare157@gmail.com>
2023-04-13 15:45:36 +0100

    add webhook for stripe payment intent

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   end
 
   post "/graphql", to: "graphql#execute"
+  post "/stripe/webhook", to: "webhook#stripe"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
